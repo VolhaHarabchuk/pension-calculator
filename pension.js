d637ee7bc70655506e2a87a3be94392309741e34
Volha Harabchuk <ioksha@gmail.com>
2020-04-22 17:02:28 +0200

    Calculate pension

---
+++ pension.js
@@ -0,0 +1,32 @@
+function getInputFloatValue (typedNumber) {
+    const input = document.getElementById(typedNumber);
+    if (!input) {
+        return 0.0;
+    }
+    return parseFloat (input.value);
+}
+
+function futureValue (principal, monthly, annualInterest, annualInflation, years, compoundsPerYear = 12) {
+    const numberOfCompounds = compoundsPerYear * years; 
+    const interestPerCompound = ((annualInterest - annualInflation) / 100.0) / compoundsPerYear;  
+    const interestPerCompound100 = 1 + interestPerCompound;
+    const power = Math.pow(interestPerCompound100, numberOfCompounds);
+    const compoundPrincipal = principal * power;
+    const compoundContributions = monthly * ((power - 1) / interestPerCompound);
+    const result = compoundPrincipal + compoundContributions;
+    return result;
+}
+function calculatePension (){
+    var initialAmount = getInputFloatValue ("intitalAmount");
+    var monthAmount = getInputFloatValue ("monthAmount");
+    var interestRate = getInputFloatValue ("interresstRate");
+    var inflationRate = getInputFloatValue ("inflationRate");
+    var numberOfYears = getInputFloatValue ("yearsToPension");
+    var pension = futureValue (initialAmount, monthAmount, interestRate, inflationRate, numberOfYears);
+    showResult(Math.round(pension));
+}
+
+function showResult(output) {
+    const result = document.getElementById("result");
+    result.innerText = output;
+}
